72455b42d385a67acbfe504053fb8b4367b9e231
Ward Abdelwahhab <wahhab.ward@gmail.com>
2023-07-15 09:11:31 +0300

    added style to ExpenseItem.js

--- Section3-React-Basics/01-starting-setup/src/components/ExpenseItem.js
+++ Section3-React-Basics/01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
   return (
-    <div>
+    <div className="expense-item">
       <div>July 15th 2023</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <div className="expense-item__price">$294.67</div>
       </div>
     </div>
   );
