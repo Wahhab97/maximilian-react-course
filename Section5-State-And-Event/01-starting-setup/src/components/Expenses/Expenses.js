48e3bdd282315e0317ce37df9c0ad566a13d7609
Ward Abdelwahhab <wahhab.ward@gmail.com>
2023-07-21 08:53:23 +0300

    Added conditional rendering

--- Section5-State-And-Event/01-starting-setup/src/components/Expenses/Expenses.js
+++ Section5-State-And-Event/01-starting-setup/src/components/Expenses/Expenses.js
@@ -15,6 +15,19 @@ const Expenses = (props) => {
     return expense.date.getFullYear() === Number(filteredYear);
   });
 
+  let expensesContent = <p>No Expenses Found!</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -22,14 +35,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onYearChange={yearChangeHanlder}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
