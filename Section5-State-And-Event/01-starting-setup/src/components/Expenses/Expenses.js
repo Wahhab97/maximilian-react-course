44fcfce0cfdfd7c22721d8fd9008eb3f67c6d3b5
Ward Abdelwahhab <wahhab.ward@gmail.com>
2023-07-21 10:27:54 +0300

    Finished section 6

--- Section5-State-And-Event/01-starting-setup/src/components/Expenses/Expenses.js
+++ Section5-State-And-Event/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
 import Card from "../UI/Card";
-import "./Expenses.css";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -22,6 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onYearChange={yearChangeHanlder}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
